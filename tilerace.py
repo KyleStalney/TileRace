6f548b65c0ac9957e7ffee794e78ec3edc31ce13
KyleStalney <kyle.alan.stanley@gmail.com>
2023-10-06 17:55:12 -0500

    cum
    what do I type

--- tilerace.py
+++ tilerace.py
@@ -163,3 +163,209 @@ DATA EXAMPLE
 	"signups": ["kylestanley"],
 }
 '''
+"Mechanical" = [{
+        "description": "complete a 500+ toa",
+        "active": False},
+    {
+        "description": "have 3 teammates win an LMS game",
+        "active": False},
+    {
+        "description": "Kill any 3 awakened bosses. You can do the same boss multiple times.",
+        "active": False},
+    {
+        "description": "Punch/Kick Jad to death",
+        "active": False},
+    {
+        "description" : "Get 5 Phosani kc in 1 trip",
+        "active": False
+    }, {
+        "description" : "Get back-to-back sub 7-minute completions of the Corrupted Gauntlet",
+        "active" : False
+    }]
+
+"Skilling" = [{
+        "description": "Get a Tome of Fire",
+        "active": False},
+    {
+        "description": "Get any Tempoross Unique. Pages that replace unique count",
+        "active": False},
+    {
+        "description": "Get an Abyssal Dye from Guardians of the Rift",
+        "active": False},
+    {
+        "description": "Acquire a Golden Tench",
+        "active": False},
+    {
+        "description" : "Acquire a Ring of Endurance",
+        "active": False},
+    {
+        "description" : "Make a Colossal Blade from scratch",
+        "active" : False},
+    {
+        "description": "Get that stupid pirate hook thing from the Brimhaven Agility Course",
+        "active": False},
+    {
+        "description": "Pickpocket 6 Blood Shards",
+        "active" : False}]
+
+"Ugh Daddy I'm Clogging" = [{
+        "description": "Obtain a Bryophyta's Essence",
+        "active": False},
+    {
+        "description": "Get a Black Tourmaline Core",
+        "active": False},
+    {
+        "description": "Get any boot drop from a medium clue",
+        "active": False},
+    {
+        "description": "Get any Revenant Weapon",
+        "active": False},
+    {
+        "description" : "Get a Champion Scroll",
+        "active": False},
+    {
+        "description" : "Obtain a complete barb assault outfit(except torso)(you can pick the hat) ",
+        "active" : False},
+    {
+        "description": "Get a Pharaoh's Sceptre",
+        "active": False},
+    {
+        "description": "Any 1 player earn 34 castle wars tickets(enough for a decorative armor set)",
+        "active" : False}]
+
+"Early Game" = [{
+        "description": "Obtain 3 Bandos Uniques",
+        "active": False},
+    {
+        "description": "Get a Barrows Double Chest",
+        "active": False},
+    {
+        "description": "Get 5 DKS rings",
+        "active": False},
+    {
+        "description": "Get any GWD Hilt",
+        "active": False},
+    {
+        "description" : "Get all Zammy uniques",
+        "active": False},
+    {
+        "description" : "Get a Dragon Pickaxe Drop",
+        "active" : False},
+    {
+        "description": "Get 3 Zenytes",
+        "active": False},
+    {
+        "description": "Get a Sarachnis Cudgel",
+        "active": False},
+    {
+        "description": "Complete a Barrows Set",
+        "active": False}]
+
+"Mid Game" = [{
+        "description": "Obtain a Voidwaker Piece",
+        "active": False},
+    {
+        "description": "Get 3 zulrah drops or a mutagen",
+        "active": False},
+    {
+        "description": "Get 5 Venator Shards. If you get one from a cache, complete the tile",
+        "active": False},
+    {
+        "description": "Get 4 Cerberus Uniques. Pegasian Crystal counts for 2.",
+        "active": False},
+    {
+        "description" : "Get that Thermy staff",
+        "active": False},
+    {
+        "description" : "Complete an Abyssal Bludgeon(as a team, specific parts don't matter)",
+        "active" : False},
+    {
+        "description": "Get an Enhanced Weapon Seed or 4 Armor Seeds",
+        "active": False},
+    {
+        "description": "Any 1 player earn 34 castle wars tickets(enough for a decorative armor set)",
+        "active" : False}]
+
+"Late Game" = [{
+        "description": "Get an Arcane or Dextrous Prayer Scroll",
+        "active": False},
+    {
+        "description": "Get a Lightbearer or Osmumten's Fang",
+        "active": False},
+    {
+        "description": "Get an Avernic Defender Hilt",
+        "active": False},
+    {
+        "description": "Get a Hydra Claw or Hydra Leather",
+        "active": False},
+    {
+        "description" : "Get a ToB Weapon",
+        "active": False},
+    {
+        "description" : "Get a CoX Weapon(butt plug counts, so does Dinny B)",
+        "active" : False},
+    {
+        "description": "Get 2 raid armor drops(Masori/Justi/Ancestral. Shields don't count)",
+        "active": False},
+    {
+        "description": "Get a Soulreaper Axe Piece",
+        "active": False},
+    {
+        "description": "Get 3 Chromium Ingots",
+        "active": False},
+    {
+        "description": "Get 2 Virtus Armor drops(can be the same piece)",
+        "active": False}]
+
+"End Game" = [{
+        "description": "Get an Inquisitor Drop(mace counts) or orb(staff doesn't count lmao)",
+        "active": False},
+    {
+        "description": "Get any Mega Rare or Dragon Claws",
+        "active": False},
+    {
+        "description": "Get 4 nex uniques. Hilt counts for 2.",
+        "active": False},
+    {
+        "description": "Get a CM or HM Tob Kit/Dust",
+        "active": False},
+    {
+        "description" : "Get 3 cox purples",
+        "active": False},
+    {
+        "description" : "Get 5 ToA purples",
+        "active" : False},
+    {
+        "description": "Get 2 Torva Drops",
+        "active": False},
+    {
+        "description": "Get 3 ToA Purples",
+        "active": False},
+    {
+        "description": "Get 3 ToB Purples",
+        "active": False},
+    {
+        "description": "Complete a Virtus Robe Set",
+        "active": False}]
+
+"MYSTERY TILE" = [{
+        "description": "Pay to win: Donate 50m to the clan or go backwards l m a o",
+        "active": False},
+    {
+        "description": "Every teammate has to get a ToB kc. It's cool if people that go out of town or whatever don't do it. Noobs have to get carried though.",
+        "active": False},
+    {
+        "description": "GRIEFER TOB: 3 members of your team must take an enemy(the enemy teams get to choose) to ToB and get a kc. The enemy must do everything they can to stop you. Literally anything goes. The same enemy cannot be chosen twice.",
+        "active": False},
+    {
+        "description": "SHANE TRAIN: Get a deathless Trio CM CoX Kc. Only one player is allowed to click in between phases at Olm. The players must form a train by following each other.",
+        "active": False},
+    {
+        "description": "Autocomplete this tile",
+        "active": False},
+    {
+        "description": "go back lmao",
+        "active": False},
+    {
+        "description": "NO PANTS INFERNOOOOOOOOOOOOOOOOOOOOOOOOO",
+        "active": False}]
